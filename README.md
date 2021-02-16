e865a57358519075fa6cad9dca01dfdfe5c51054
Jamie Bukraba <Jamie.Bukraba@googlemail.com>
2021-02-16 22:32:33 +0000

    added win conditions. switched to array from hash

---
+++ README.md
@@ -0,0 +1,1 @@
+Command line noughts and crosses game use 1 to 9 to place noughts and crosses into grid get a line of 3 to win
--- noughts_and_crosses.rb
+++ noughts_and_crosses.rb
@@ -1,39 +1,61 @@
 require 'pry'
 class GameBoard
   def initialize
-    @board =
-      [{
-        position1: 1,
-        position2: 2,
-        position3: 3,
-        position4: 4,
-        position5: 5,
-        position6: 6,
-        position7: 7,
-        position8: 8,
-        position9: 9
-      }]
+    @board = [*1...10]
   end
 
+  WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+    [2, 4, 6],
+    [0, 4, 8]
+  ]
   def print_board
-    puts "\n   #{@board[:position1]} | #{@board[:position2]} | #{@board[:position3]}"
+    puts "\n   #{@board[0]} | #{@board[1]} | #{@board[2]}"
     puts '  -----------'
-    puts "   #{@board[:position4]} | #{@board[:position5]} | #{@board[:position6]}"
+    puts "   #{@board[3]} | #{@board[4]} | #{@board[5]}"
     puts '  -----------'
-    puts "   #{@board[:position7]} | #{@board[:position8]} | #{@board[:position9]} \n"
+    puts "   #{@board[6]} | #{@board[7]} | #{@board[8]} \n"
   end
 
   def place_piece(icon)
     number = check_int(gets.chomp)
-    @board[('position' + number.to_s).to_sym] = icon
+    @board[number.to_i - 1] = icon
   end
 
-  def check_win; end
+  def check_win
+    WINNING_COMBINATIONS.each do |combo|
+      if [@board[combo[0]], + @board[combo[1]], @board[combo[2]]].uniq.length == 1
+        @winner = @board[combo[0]]
+        return true
+      end
+    end
+    unless @board.any? { |i| i.is_a? Integer }
+      @winner = draw
+      true
+    end
+  end
+
+  def winner_announce(player1, player2)
+    case @winner
+    when 'O'
+      puts "#{player1.name} wins. Congratulations"
+    when 'X'
+      puts "#{player2.name} wins. Congratulations"
+    when 'Draw'
+      puts "It's a tie, what else did you expect?"
+    else puts 'Well done breaking the game'
+    end
+  end
 
   private
 
   def check_int(integer)
-    until @board[('position' + integer.to_s).to_sym] == integer.to_i
+    until @board[integer.to_i - 1] == integer.to_i
       puts 'Enter unused number between 1 and 9'
       integer = gets.chomp
     end
@@ -57,17 +79,27 @@ def game_start
   puts 'Player 2 Enter Name'
   player2 = Player.new(gets.chomp, 'X')
   until game.check_win
-    puts 'Player 1 select space'
+    puts "#{player1.name} select space"
     game.print_board
     game.place_piece(player1.icon)
-    game.check_win
-    puts 'Player 2 select space'
+    break if game.check_win
+
+    puts "#{player2.name} select space"
     game.print_board
     game.place_piece(player2.icon)
   end
+  game.print_board
+  game.winner_announce(player1, player2)
 end
+
 game_start
-until gets.chomp.downcase == 'n'
-  puts 'want to play again?'
+puts 'want to play again? Y/N'
+if gets.chomp.downcase == 'y'
   game_start
+  puts 'want to play again? Y/N'
+  until gets.chomp.downcase == 'n'
+    game_start
+    puts 'want to play again? Y/N'
+  end
 end
+puts 'Thank you for playing'
